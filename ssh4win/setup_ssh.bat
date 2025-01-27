3812ea79e190eada43df47a9ed9e3ec272519814
capt-sms <captaccs@pm.me>
2025-01-27 07:28:28 +0100

    Update setup_ssh.bat

--- ssh4win/setup_ssh.bat
+++ ssh4win/setup_ssh.bat
@@ -5,7 +5,7 @@ set "log_file=%~dp0setup_ssh.log"
 echo Logging to %log_file%
 > "%log_file%" 2>&1 (
     if "%1"=="" (
-        echo Usage: %0 home_dir [filepaths, ...]
+        echo Usage: %0 home_dir
         exit /b 101
     )
 
@@ -47,20 +47,21 @@ echo Logging to %log_file%
         copy /y %source_dir%%%f %home_dir%\%%f
         @echo off
     )
-
+    
     echo Processing user private keys...
-
-    for %%f in (%priv_home%) do (
-        echo from %source_dir%%%f to %home_dir%\%%f
-        @echo on
-        copy /y %source_dir%%%f %home_dir%\%%f
-        @echo off
-
-        icacls %home_dir%\%%~nf /inheritance:r
-        icacls %home_dir%\%%~nf /setowner %USERNAME%
-        icacls %home_dir%\%%~nf /grant SYSTEM:R Administrators:R %USERNAME%:F
-        icacls %home_dir%\%%~nf /grant:r "Authenticated Users":RX Users:RX
-    )
+    
+        for %%f in (%priv_home%) do (
+            echo from %source_dir%%%f to %home_dir%\%%f
+            @echo on
+            copy /y %source_dir%%%f %home_dir%\%%f
+            @echo off
+            
+            icacls %home_dir%\%%~nf /inheritance:r
+            icacls %home_dir%\%%~nf /setowner %USERNAME%
+    
+            icacls %home_dir%\%%~nf /grant SYSTEM:R Administrators:R %USERNAME%:F
+            icacls %home_dir%\%%~nf /grant:r "Authenticated Users":RX Users:RX
+        )
 
     echo Processing root config...
 
@@ -71,25 +72,10 @@ echo Logging to %log_file%
         @echo off
 
         icacls %root_dir%\%%~nf /inheritance:r
+
         icacls %root_dir%\%%~nf /grant SYSTEM:F Administrators:F
     )
 
-    shift
-    :process_files
-    if "%~1"=="" goto :done
-
-    set "file_path=%~1"
-    echo Processing file: %file_path%
-
-    icacls "%file_path%" /inheritance:r
-    icacls "%file_path%" /setowner %USERNAME%
-    icacls "%file_path%" /grant SYSTEM:R Administrators:R %USERNAME%:F
-    icacls "%file_path%" /grant:r "Authenticated Users":RX Users:RX
-
-    shift
-    goto :process_files
-
-    :done
-    echo Files permissions set successfully.
+    echo Files copied and permissions set successfully.
 )
 exit /b 0
