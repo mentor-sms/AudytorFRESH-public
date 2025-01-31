f26e7b911adfbc2eb091d77c555dcae4178bb9d2
capt-sms <captaccs@pm.me>
2025-01-31 03:14:10 +0100

    Update setup_sshd.bat

--- sshd4win/setup_sshd.bat
+++ sshd4win/setup_sshd.bat
@@ -2,87 +2,62 @@
 setlocal enabledelayedexpansion
 
 set "log_file=%~dp0setup_sshd.log"
-echo Logging to %log_file%
+echo [setup_sshd] Logging to %log_file%
 > "%log_file%" 2>&1 (
-    REM Check if the username argument is provided
-    if "%1"=="" (
-        echo Usage: %0 username
+    if "%~1"=="" (
+        echo [setup_sshd] Usage: %0 home_dir
         exit /b 101
     )
 
-    REM Set the username and home_dir based on the provided argument
-    set "home_dir=%1"
+    set home_dir=%~1\.ssh
+    set source_dir=%~dp0
+    set root_dir=C:\ProgramData\ssh
 
-    REM Define source and destination directories
-    set "source_dir=%~dp0"
-    set "root_dir=C:\ProgramData\ssh"
+    echo [setup_sshd] "SSH directory: !root_dir!"
+    echo [setup_sshd] "Source directory: !source_dir!"
+    echo [setup_sshd] "Home directory: !home_dir!"
 
-    REM Ensure the script is running with administrator privileges
-    >nul 2>&1 "%SystemRoot%\system32\cacls.exe" "%SystemRoot%\system32\config\system"
+    echo [setup_sshd] %SystemRoot%\system32\cacls.exe
+    echo [setup_sshd] %SystemRoot%\system32\config\system
+    >nul 2>&1 %SystemRoot%\system32\cacls.exe %SystemRoot%\system32\config\system
     if %errorlevel% neq 0 (
         echo This script requires elevated privileges. Please run as administrator.
         exit /b 102
     )
 
-    REM Define the list of files to process
-    set files_root_only=sshd_config.txt
-    set files_non_txt=id_ed25519.pub id_rsa.pub
-    set files_private=id_ed25519 id_rsa
+    echo [setup_sshd] Elevated privileges verified.
 
-    REM Process non-txt files to be copied to root_dir with modified names
-    for %%f in (%files_non_txt%) do (
-        set "dest_file=%%~nf"
+    set txt_home=known_hosts.txt config.txt
+    set pub_home=id_ed25519.pub id_rsa.pub
+    set priv_home=id_ed25519 id_rsa
+    set txt_root=sshd_config.txt
 
-        if "%%f"=="dev_ed25519.pub" set "dest_file=dev_ed25519.pub"
-        if "%%f"=="id_ed25519.pub" set "dest_file=ssh_host_ed25519_key.pub"
-        if "%%f"=="id_rsa.pub" set "dest_file=ssh_host_rsa_key.pub"
-
-        REM Use delayed expansion for dest_file
-        set "current_dest_file=!dest_file!"
-
-        REM Copy file to root_dir with modified name
-        copy /y "%source_dir%\%%f" "%root_dir%\!current_dest_file!"
-        if !errorlevel! neq 0 (
-            echo Error encountered during copying %%f to %root_dir%\!current_dest_file!. Error code: !errorlevel!
-            exit /b 103
-        )
+    echo [setup_sshd] Processing user config...
+    for %%f in (%txt_home%) do (
+        echo [setup_sshd] from !source_dir!%%f to !home_dir!\%%~nf
+        copy /y "!source_dir!%%f" "!home_dir!\%%~nf"
     )
 
-    REM Process private files to be copied to root_dir with modified names
-    for %%f in (%files_private%) do (
-        set "dest_file=%%~nf"
-
-        if "%%f"=="id_ed25519" set "dest_file=ssh_host_ed25519_key"
-        if "%%f"=="id_rsa" set "dest_file=ssh_host_rsa_key"
-
-        REM Use delayed expansion for dest_file
-        set "current_dest_file=!dest_file!"
-
-        REM Copy file to root_dir with modified name
-        copy /y "%source_dir%\%%f" "%root_dir%\!current_dest_file!"
-        if !errorlevel! neq 0 (
-            echo Error encountered during copying %%f to %root_dir%\!current_dest_file!. Error code: !errorlevel!
-            exit /b 104
-        )
+    echo [setup_sshd] Processing user public keys...
+    for %%f in (%pub_home%) do (
+        echo [setup_sshd] from !source_dir!%%f to !home_dir!\%%f
+        copy /y !source_dir!%%f !home_dir!\%%f
     )
 
-    REM Process files to be copied only to root_dir with new names
-    for %%f in (%files_root_only%) do (
-        set "dest_file=%%~nf"
+    echo [setup_sshd] Processing user private keys...
 
-        if "%%f"=="sshd_config.txt" set "dest_file=sshd_config"
-
-        REM Use delayed expansion for dest_file
-        set "current_dest_file=!dest_file!"
-
-        REM Copy file to root_dir with modified name
-        copy /y "%source_dir%\%%f" "%root_dir%\!current_dest_file!"
-        if %errorlevel% neq 0 (
-            echo Error encountered during copying %%f to %root_dir%\!current_dest_file!. Error code: %errorlevel%
-            exit /b 105
-        )
+    for %%f in (%priv_home%) do (
+        echo [setup_sshd] from !source_dir!%%f to !home_dir!\%%f
+        copy /y !source_dir!%%f !home_dir!\%%f
     )
 
-    echo Files copied successfully.
+    echo [setup_sshd] Processing root config...
+
+    for %%f in (%txt_root%) do (
+        echo [setup_sshd] from !source_dir!%%f to !home_dir!\%%~nf
+        copy /y !source_dir!%%f !root_dir!\%%~nf
+    )
+
+    echo [setup_sshd] Files copied and permissions set successfully.
 )
 exit /b 0
