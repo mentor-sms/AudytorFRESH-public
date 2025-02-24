88d0ef37a275019b99bc9b49fec684b82ec468c1
Szymon M. Sabat <captaccs@pm.me>
2025-02-24 09:02:47 +0100

    Update copy4prepare.sh

--- home4copy/copy4prepare.sh
+++ home4copy/copy4prepare.sh
@@ -67,7 +67,7 @@ run_rsync() {
     echo "Executing: $rsync_cmd"
     eval "$rsync_cmd" | while read -r line; do
         fullname="$target$line"
-        fullname="${fullname///$mnt\/$home_dir//\//}"
+        fullname="${fullname//$mnt\/$home_dir\//}"
         echo "$fullname"
         if echo "$fullname" | grep -q '^[0-9a-zA-Z._/]*[0-9a-zA-Z]$'; then
             echo "> $line"
