b21b146dda633cd8b2b865543944ceae17bc46fe
Szymon M. Sabat <captaccs@pm.me>
2025-06-05 02:58:04 +0200

    zxc

--- home4copy/copy4prepare.sh
+++ home4copy/copy4prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-WERSJA=0.7.2
+WERSJA=1.0.0 #!=
 SECONDS_START=$(date +%s)
 do_umount=0                          # Flag to track if we mounted a device
 from="USB"                       # Source location (block device or directory)
