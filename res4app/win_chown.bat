0e21958e2e71021b8875cafb3a8acc77dadf4cf7
Szymon M. Sabat <captaccs@pm.me>
2025-08-26 13:35:59 +0200

    as

--- res4app/win_chown.bat
+++ res4app/win_chown.bat
@@ -1,118 +1,72 @@
 @echo on
 ver >nul 2>&1
 
-set "MODE=%~1"
-set "ID=%~2"
-set "LOG_DIR=%~3"
-set "SID=%~5"
-
-set "log_file=%LOG_DIR%\cmd_%id%.run.lab.log"
+set "FILE=%~1"
+set "MODE=%~2"
+set "SID=%~3"
+set "LOG_FILE=%~4"
 
 setlocal EnableDelayedExpansion
 
-if !errorlevel! neq 0 echo init errorlevel non-zero>> "!log_file!" & exit /b 10
+if !errorlevel! neq 0 echo init errorlevel non-zero>> "%LOG_FILE%" & exit /b 10
 
 (
   echo "REV3 BAT"
-  echo "ID !ID!"
-  echo "SID !SID!"
+  echo "ID %ID%"
+  echo "SID %SID%"
   echo "%cmdcmdline%"
   echo "%~f0" "%*"
-  echo "MODE !MODE!"
-)>> "!log_file!" 2>&1
+  echo "MODE %MODE%"
+)>> "%LOG_FILE%" 2>&1
 
-if !errorlevel! neq 0 echo failed to write header to log>> "!log_file!" & exit /b 11
+if !errorlevel! neq 0 echo failed to write header to log>> "%LOG_FILE%" & exit /b 11
 
-set "arg_index=0"
-set "file_cnt=0"
-for %%A in (%*) do (
-  set /a arg_index+=1
-  if !arg_index! GEQ 6 (
-    set /a file_cnt+=1
-    set "FILE_!file_cnt!=%%~A"
+echo WORK file "%FILE%">> "%LOG_FILE%"
+
+icacls "%FILE%" /reset
+set "rc=!ERRORLEVEL!"
+if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 22
+
+if /i "%MODE%"=="private" (
+
+  icacls "%FILE%" /grant *%SID%:F
+  set "rc=!ERRORLEVEL!"
+  if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 23
+
+  icacls "%FILE%" /setowner *%SID%
+  set "rc=!ERRORLEVEL!"
+  if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 24
+
+  icacls "%FILE%" /inheritance:r /c /grant:r *%SID%:F
+  set "rc=!ERRORLEVEL!"
+  if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 25
+
+) else if not /i "%MODE%"=="default" (
+
+  icacls "%FILE%" /grant *S-1-5-32-544:F
+  set "rc=!ERRORLEVEL!"
+  if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 26
+
+  icacls "%FILE%" /setowner *S-1-5-32-544
+  set "rc=!ERRORLEVEL!"
+  if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 27
+
+  if /i "%MODE%"=="root_private" (
+
+    icacls "%FILE%" /inheritance:r /c /grant:r *S-1-5-18:F *S-1-5-32-544:F
+    set "rc=!ERRORLEVEL!"
+    if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 28
+
+  ) else if /i "%MODE%"=="root_public" (
+
+    icacls "%FILE%" /inheritance:r /c /grant:r *S-1-5-18:F *S-1-5-32-544:F *S-1-5-11:RX *S-1-5-32-545:RX
+    set "rc=!ERRORLEVEL!"
+    if not "!rc!"=="0" echo icaclserr !rc!>> "%LOG_FILE%" & exit /b 29
   )
-  echo !file_cnt! FILE "!_FP!">> "!log_file!"
 )
 
-if !errorlevel! neq 0 echo args enumeration failed>> "!log_file!" & exit /b 12
+if !errorlevel! neq 0 echo post-file processing error for "%FILE%">> "%LOG_FILE%" & exit /b 15
 
-for /L %%N in (1,1,!file_cnt!) do (
-  setlocal DisableDelayedExpansion
-  call set "_FP=%%FILE_%%N%%"
-  endlocal & set "_FP=%_FP%"
-
-  if !errorlevel! neq 0 echo failed to resolve file path index %%N>> "!log_file!" & exit /b 21
-
-  echo WORK file "!_FP!">> "!log_file!"
-
-  setlocal DisableDelayedExpansion
-  icacls "%_FP%" /reset
-  call set "rc=%%ERRORLEVEL%%"
-  endlocal & set "rc=%rc%"
-
-  if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 22
-
-  if /i "%MODE%"=="private" (
-
-    setlocal DisableDelayedExpansion
-    icacls "%_FP%" /grant *%SID%:F
-    call set "rc=%%ERRORLEVEL%%"
-    endlocal & set "rc=%rc%"
-    if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 23
-
-    setlocal DisableDelayedExpansion
-    icacls "%_FP%" /setowner *%SID%
-    call set "rc=%%ERRORLEVEL%%"
-    endlocal & set "rc=%rc%"
-    if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 24
-
-    setlocal DisableDelayedExpansion
-    icacls "%_FP%" /inheritance:r /c /grant:r *%SID%:F
-    call set "rc=%%ERRORLEVEL%%"
-    endlocal & set "rc=%rc%"
-    if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 25
-
-  ) else if not /i "%MODE%"=="default" (
-
-    setlocal DisableDelayedExpansion
-    icacls "%_FP%" /grant *S-1-5-32-544:F
-    call set "rc=%%ERRORLEVEL%%"
-    endlocal & set "rc=%rc%"
-    if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 26
-
-    setlocal DisableDelayedExpansion
-    icacls "%_FP%" /setowner *S-1-5-32-544
-    call set "rc=%%ERRORLEVEL%%"
-    endlocal & set "rc=%rc%"
-    if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 27
-
-    if /i "%MODE%"=="root_private" (
-
-      setlocal DisableDelayedExpansion
-      icacls "%_FP%" /inheritance:r /c /grant:r *S-1-5-18:F *S-1-5-32-544:F
-      call set "rc=%%ERRORLEVEL%%"
-      endlocal & set "rc=%rc%"
-      if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 28
-
-    ) else if /i "%MODE%"=="root_public" (
-
-      setlocal DisableDelayedExpansion
-      icacls "%_FP%" /inheritance:r /c /grant:r *S-1-5-18:F *S-1-5-32-544:F *S-1-5-11:RX *S-1-5-32-545:RX
-      call set "rc=%%ERRORLEVEL%%"
-      endlocal & set "rc=%rc%"
-      if not "!rc!"=="0" echo icaclserr !rc!>> "!log_file!" & exit /b 29
-    )
-  )
-
-  if !errorlevel! neq 0 echo post-file processing error for "%_FP%">> "!log_file!" & exit /b 15
-
-  echo OK file "!_FP!">> "!log_file!"
-)
-
-if !errorlevel! neq 0 (
-  echo loop termination error>> "!log_file!"
-  exit /b 19
-)
-
-echo OKFIN>> "!log_file!"
+echo OK file "%FILE%">> "%LOG_FILE%"
+echo OKFIN>> "%LOG_FILE%"
 exit /b 0
--- res4app/win_chown.cmd
+++ res4app/win_chown.cmd
@@ -2,136 +2,130 @@
 @echo off
 ver >nul 2>&1
 
-set "MODE=%~1"
-set "ID=%~2"
-set "LOG_DIR=%~3"
-set "SID=%~5"
+REM win_chown.cmd FILE MODE=default [SID] ID=0 LOG_DIR=workdir
+
+
+REM ARGC:
+
+set "ARGC=0"
+set "IS_INT="
+if not "%1"=="" set "ARGC=1"
+if not "%2"=="" set "ARGC=2"
+if not "%3"=="" set "ARGC=3" & set "IS_INT=%~3"
+if not "%4"=="" set "ARGC=4" & set "IS_INT=%~4"
+if not "%5"=="" set "ARGC=5"
+if not "%~6"=="" (
+    echo too many arguments>&2
+    exit /b 5
+)
+
+if %ARGC% LSS 2 (
+    echo too few arguments>&2
+    exit /b 2
+)
+
+if defined IS_INT (
+  rem IS_INT
+    set "r=%IS_INT%"
+    set "IS_INT="
+    if "%r:~0,1%"=="-" set "r=%r:~1%"
+    if "%r:~0,1%"=="+" set "r=%r:~1%"
+    set "nd=x"
+    for /f "tokens=1 delims=0123456789" %%A in ("%r%") do set "nd=%%A"
+    set "IS_INT="
+    if not "%r%"=="" if "%nd%"=="x" set "IS_INT=1"
+  rem IS_INT FIN
+)
+
+
+REM ARGS:
+
+set "FILE=%~1"
+if "%FILE%"=="" (
+  echo missing FILE>&2
+  exit /b 1
+)
+
+set "MODE=%~2"
+
+if not "%3"=="" (
+  if "%4"=="" (
+    if defined IS_INT (
+      set "ID=%~3"
+    ) else (=
+      set "SID=%~3"
+    )
+  ) else if "%5"=="" (
+    if defined IS_INT (
+      set "ID=%~3"
+      set "LOG_DIR=%~4"
+    ) else (
+      set "SID=%~3"
+      set "ID=%~4"
+    )
+  ) else (
+    set "SID=%~3"
+    set "ID=%~4"
+    set "LOG_DIR=%~5"
+  )
+)
+
+
+REM DEFS:
+
+if "%ID%"=="" set "ID=0"
+if "%MODE%"=="" set "MODE=default"
+if "%LOG_DIR%"=="" set "LOG_DIR=%cd%"
 
 set "log_file=%LOG_DIR%\cmd_%id%.run.lab.log"
 
-if errorlevel 1 (
-    echo init errorlevel non-zero
-    exit /b 10
-)
+setlocal EnableDelayedExpansion
 
-if "%~6"=="" (
-    echo missing required argument 6
+if "!MODE!"=="private" if "!SID!"=="" (
+    echo missing SID for private mode>&2
     exit /b 6
 )
 
-setlocal EnableDelayedExpansion
+if !ERRORLEVEL! neq 0 (
+    echo internal error>&2
+    exit /b 7
+)
 
-if /i not "!MODE!"=="default" if /i not "!MODE!"=="private" if /i not "!MODE!"=="root_private" if /i not "!MODE!"=="root_public" (
-    echo invalid MODE "!MODE!"
-    exit /b 1
-)
-if "!ID!"=="" (
-    echo missing ID
-    exit /b 2
-)
-if "!LOG_DIR!"=="" (
-    echo missing LOG_DIR
-    exit /b 3
-)
-if /i "!MODE!"=="private" (
-    if "!SID!"=="" (
-        echo missing SID for private mode
-        exit /b 5
-    )
-)
+
+REM WORK
 
 echo "REV3 CMD"
-echo "ID !ID!"
-echo "SID !SID!"
-echo "%cmdcmdline%"
-echo "%~f0" "%*"
-echo "MODE !MODE!"
-
-set "ARG_START_IDX=6"
-set "arg_idx=0"
-set "FILE_CNT=0"
-set "PS_ARGS="
-
-if !errorlevel! neq 0 (
-    echo internal error before args loop
-    exit /b 11
-)
-
-for %%A in (%*) do (
-
-    set /a arg_idx+=1
-
-    setlocal DisableDelayedExpansion
-    set "curr_arg=%%~A"
-    endlocal & set "curr_arg=%curr_arg%"
-
-    echo !arg_idx! FILE "!curr_arg!"
-
-    set "CUR_ESC=!curr_arg:'='''!"
-
-    if defined PS_ARGS (
-        set "PS_ARGS=!PS_ARGS!,'!CUR_ESC!'"
-    ) else (
-        set "PS_ARGS='!CUR_ESC!'"
-    )
-
-    if !arg_idx! geq !ARG_START_IDX! (
-        set /a FILE_CNT+=1
-        set "FILE_!FILE_CNT!=!curr_arg!"
-
-        if "!curr_arg!"=="" (
-            echo empty path argument
-            exit /b 21
-        )
-        if not exist "!curr_arg!" (
-            echo path does not exist: "!curr_arg!"
-            exit /b 22
-        )
-
-        icacls "!curr_arg!" >nul 2>&1
-        if !errorlevel! neq 0 (
-            echo icacls test failed for "!curr_arg!"
-            exit /b 23
-        )
-
-        dir "!curr_arg!" >nul 2>&1
-        if !errorlevel! neq 0 (
-            echo dir test failed for "!curr_arg!"
-            exit /b 24
-        )
-    )
-)
-
-if !errorlevel! neq 0 (
-    echo internal error after args loop
-    exit /b 12
-)
+echo "ID %ID%"
+echo COMMAND %~f0 %*
+echo CMDLINE %cmdcmdline%
+echo "MODE %MODE%"
 
 setlocal DisableDelayedExpansion
+
 echo INBAT
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "$a = @(%PS_ARGS%); $p = Start-Process -Verb RunAs -FilePath '%~dpn0.bat' -ArgumentList $a -PassThru; $p.WaitForExit(); exit $p.ExitCode"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "$a = @('%MODE%','%FILE%','%SID%','%log_file%'); $p = Start-Process -Verb RunAs -FilePath '%~dpn0.bat' -ArgumentList $a -PassThru; $p.WaitForExit(); exit $p.ExitCode"
 echo OUTBAT
-if errorlevel 70001 (
-    echo wrapper failed
+if ERRORLEVEL 70001 (
+    echo wrapper failed>&2
     exit /b 91
 )
-if errorlevel 1224 (
-    echo elevation failed
+if ERRORLEVEL 1224 (
+    echo elevation failed>&2
     exit /b 92
-) else if errorlevel 1223 (
-    echo user canceled
+) else if ERRORLEVEL 1223 (
+    echo user canceled>&2
     exit /b 93
-) else if errorlevel 1 (
+) else if ERRORLEVEL 1 (
     setlocal EnableDelayedExpansion
-    set "BC=!errorlevel!"
-    echo BAT err !BC!
+    set "BC=!ERRORLEVEL!"
+    echo BAT err !BC!>&2
     set /a RC=BC+100
     exit /b !RC!
 )
 endlocal
 
-if !errorlevel! neq 0 (
-    echo internal error after elevation handling
+if !ERRORLEVEL! neq 0 (
+    echo internal error after elevation handling>&2
     exit /b 13
 )
 
@@ -139,8 +133,9 @@ echo OKFIN
 
 timeout /t 30 /nobreak >nul
 
-if !errorlevel! neq 0 (
-    echo timeout failed
+if !ERRORLEVEL! neq 0 (
+    echo timeout failed>&2
+
     exit /b 19
 )
 
